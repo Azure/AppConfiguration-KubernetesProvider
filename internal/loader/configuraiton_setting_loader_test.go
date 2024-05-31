cd14fae529a3d7dfde95b3cd64e7e7493f6121cb
Lingling Ye (from Dev Box) <linglingye@microsoft.com>
2024-05-31 16:29:47 +0800

    deduplicate feature flags

--- internal/loader/configuraiton_setting_loader_test.go
+++ internal/loader/configuraiton_setting_loader_test.go
@@ -68,6 +68,15 @@ func mockConfigurationSettingsWithKV() []azappconfig.Setting {
 	return settingsToReturn
 }
 
+func mockFeatureFlagSettings() []azappconfig.Setting {
+	settingsToReturn := make([]azappconfig.Setting, 3)
+	settingsToReturn[0] = newFeatureFlagSettings(".appconfig.featureflag/Beta", "label1")
+	settingsToReturn[1] = newFeatureFlagSettings(".appconfig.featureflag/Alpha", "label1")
+	settingsToReturn[2] = newFeatureFlagSettings(".appconfig.featureflag/Beta", "label1")
+
+	return settingsToReturn
+}
+
 func newCommonKeyValueSettings(key string, value string, label string) azappconfig.Setting {
 	return azappconfig.Setting{
 		Key:   &key,
@@ -85,6 +94,18 @@ func newKeyVaultSettings(key string, label string) azappconfig.Setting {
 		Value:       &vault,
 		Label:       &label,
 		ContentType: &keyVaultContentType,
+	}
+}
+
+func newFeatureFlagSettings(key string, label string) azappconfig.Setting {
+	featureFlagContentType := FeatureFlagContentType
+	featureFlagValue := "\"conditions\":{},\"description\":\"\",\"enabled\":false,\"id\":\"fakeId\""
+
+	return azappconfig.Setting{
+		Key:         &key,
+		Value:       &featureFlagValue,
+		Label:       &label,
+		ContentType: &featureFlagContentType,
 	}
 }
 
@@ -943,6 +964,54 @@ var _ = Describe("AppConfiguationProvider Get All Settings", func() {
 			Expect(allSettings.ConfigMapSettings["someSubKey1:1"]).Should(Equal("value4"))
 			Expect(allSettings.ConfigMapSettings["test:some"]).Should(Equal("value5"))
 			Expect(allSettings.ConfigMapSettings["test:"]).Should(Equal("value6"))
+		})
+
+		It("Succeed to get all configuration settings", func() {
+			By("By loading key values and feature flags")
+			featureFlagKeyFilter := "*"
+			testSpec := acpv1.AzureAppConfigurationProviderSpec{
+				Endpoint:                &EndpointName,
+				ReplicaDiscoveryEnabled: false,
+				Target: acpv1.ConfigurationGenerationParameters{
+					ConfigMapName: ConfigMapName,
+					ConfigMapData: &acpv1.ConfigMapDataOptions{
+						Type: acpv1.Json,
+						Key:  "settings.json",
+					},
+				},
+				Configuration: acpv1.AzureAppConfigurationKeyValueOptions{
+					TrimKeyPrefixes: []string{"app:", "test:"},
+				},
+				FeatureFlag: &acpv1.AzureAppConfigurationFeatureFlagOptions{
+					Selectors: []acpv1.Selector{
+						{
+							KeyFilter: &featureFlagKeyFilter,
+						},
+					},
+				},
+			}
+			testProvider := acpv1.AzureAppConfigurationProvider{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "azconfig.io/v1",
+					Kind:       "AppConfigurationProvider",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "testName",
+					Namespace: "testNamespace",
+				},
+				Spec: testSpec,
+			}
+
+			settingsToReturn := mockConfigurationSettings()
+			featureFlagsToReturn := mockFeatureFlagSettings()
+			mockSettingsClient.EXPECT().GetSettings(gomock.Any(), gomock.Any()).Return(settingsToReturn, nil)
+			mockSettingsClient.EXPECT().GetSettings(gomock.Any(), gomock.Any()).Return(featureFlagsToReturn, nil)
+			mockCongiurationClientManager.EXPECT().GetClients(gomock.Any()).Return([]*ConfigurationClientWrapper{&fakeClientWrapper}, nil)
+			configurationProvider, _ := NewConfigurationSettingLoader(testProvider, mockCongiurationClientManager, mockSettingsClient)
+			allSettings, err := configurationProvider.CreateTargetSettings(context.Background(), mockResolveSecretReference)
+
+			Expect(err).Should(BeNil())
+			Expect(len(allSettings.ConfigMapSettings)).Should(Equal(1))
 		})
 
 		It("Fail to get all configuration settings", func() {

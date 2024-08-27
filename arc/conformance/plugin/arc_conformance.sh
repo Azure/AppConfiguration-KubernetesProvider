c537835ff19e9711c1236ff2f31b7c71c23cd2b3
Lingling Ye (from Dev Box) <linglingye@microsoft.com>
2024-08-27 14:46:13 +0800

    test in managed cluster

--- arc/conformance/plugin/arc_conformance.sh
+++ arc/conformance/plugin/arc_conformance.sh
@@ -4,28 +4,98 @@ set -e
 
 results_dir="${RESULTS_DIR:-/tmp/results}"
 
-waitForArc() {
-    ready=false
-    max_retries=60
-    sleep_seconds=20
+cluster_type_connected="managedClusters"
 
+# validate enviorment variables
+validateParameters() {
+  if [ -z "${TENANT_ID}" ]; then
+    echo "ERROR: parameter TENANT_ID is required." > "${results_dir}"/error
+    python3 /arc/setup_failure_handler.py
+  fi
+
+  if [ -z "${SUBSCRIPTION_ID}" ]; then
+    echo "ERROR: parameter SUBSCRIPTION_ID is required." > "${results_dir}"/error
+    python3 /arc/setup_failure_handler.py
+  fi
+
+  if [ -z $CLUSTER_TYPE ]; then
+		echo "CLUSTER_TYPE is undefined, defaulting to managedClusters" 
+		CLUSTER_TYPE=$cluster_type_connected
+	fi
+
+  if [ -z "${CLUSTER_NAME}" ]; then
+    echo "ERROR: parameter CLUSTER_NAME is required." > "${results_dir}"/error
+    python3 /arc/setup_failure_handler.py
+  fi
+
+  if [ -z "${CLUSTER_RG}" ]; then
+    echo "ERROR: parameter CLUSTER_RG is required." > "${results_dir}"/error
+    python3 /arc/setup_failure_handler.py
+  fi
+
+  if [[ -z "${WORKLOAD_CLIENT_ID}" ]]; then
+      echo "ERROR: parameter AZURE_CLIENT_ID is required." > "${results_dir}"/error
+      python3 /arc/setup_failure_handler.py
+  fi
+}
+
+login_to_azure() {
+	# Login with managed identity
+  echo "login to azure using managed identity: '${WORKLOAD_CLIENT_ID}'"
+	az login --identity \
+	--username ${WORKLOAD_CLIENT_ID} 2> ${results_dir}/error || python3 setup_failure_handler.py || exit 1
+
+	echo "setting subscription: ${SUBSCRIPTION_ID} as default subscription"
+	az account set -s $SUBSCRIPTION_ID
+}
+
+addK8sExtension() {
+  echo "adding k8s-extension extension"
+  az extension add --name k8s-extension
+
+  # register the KubernetesConfiguration resource provider
+  az provider register --namespace Microsoft.KubernetesConfiguration
+}
+
+createK8sProviderExtension() {
+	echo "INFO: Creating Microsoft.AppConfiguration extension"
+  az k8s-extension create \
+    --name appconfigurationkubernetesprovider \
+    --extension-type Microsoft.AppConfiguration \
+    --cluster-name "${CLUSTER_NAME}" \
+    --resource-group "${CLUSTER_RG}" \
+    --cluster-type "${$CLUSTER_TYPE}" \
+    --release-train preview 2> "${results_dir}"/error || python3 /arc/setup_failure_handler.py
+}
+
+waitForK8sProviderExtensionInstalled() {
+    installedState=false
+    max_retries=40
+    sleep_seconds=10
     for i in $(seq 1 $max_retries)
     do
-    status=$(helm ls -a -A -o json | jq '.[]|select(.name=="azure-arc").status' -r)
-    if [ "$status" == "deployed" ]; then
-        echo "helm release successful"
-        ready=true
-        break
-    elif [ "$status" == "failed" ]; then
-        echo "helm release failed"
-        break
-    else
-        echo "waiting for helm release to be successful. Status - ${status}. Attempt# $i of $max_retries"
-        sleep ${sleep_seconds}
-    fi
+      echo "iteration: ${i}, clustername: ${CLUSTER_NAME}, resourcegroup: ${CLUSTER_RG}"
+      provisioningState=$(az k8s-extension show  --cluster-name $CLUSTER_NAME --resource-group $CLUSTER_RG --cluster-type $CLUSTER_TYPE --name appconfigurationkubernetesprovider --query provisioningState -o json)
+      provisioningState=$(echo $provisioningState | tr -d '"' | tr -d '"\r\n')
+      echo "extension provisioning state: ${provisioningState}"
+      if [ ! -z "$provisioningState" ]; then
+         if [ "${provisioningState}" == "Succeeded" ]; then
+            break
+         fi
+      fi
+      sleep ${sleep_seconds}
     done
+    echo "Microsoft.AppConfiguration extension installed"
+}
 
-    echo "$ready"
+deleteK8sProviderExtension() {
+  az k8s-extension delete \
+    --name appconfigurationkubernetesprovider \
+    --resource-group "${CLUSTER_RG}" \
+    --cluster-type "${$CLUSTER_TYPE}" \
+    --cluster-name "${CLUSTER_NAME}" \
+    --force \
+    --yes
 }
 
 saveResult() {
@@ -40,135 +110,20 @@ saveResult() {
 # Ensure that we tell the Sonobuoy worker we are done regardless of results.
 trap saveResult EXIT
 
-# setup kubeconfig for conformance test
-setupKubeConfig() {
-  KUBECTL_CONTEXT=azure-arc-appconfig-test
-  APISERVER=https://kubernetes.default.svc/
-  TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-  cat /var/run/secrets/kubernetes.io/serviceaccount/ca.crt > ca.crt
-
-  kubectl config set-cluster ${KUBECTL_CONTEXT} \
-    --embed-certs=true \
-    --server=${APISERVER} \
-    --certificate-authority=./ca.crt 2> "${results_dir}"/error || python3 /arc/setup_failure_handler.py
-
-  kubectl config set-credentials ${KUBECTL_CONTEXT} --token="${TOKEN}" 2> "${results_dir}"/error || python3 /arc/setup_failure_handler.py
-
-  # Delete previous rolebinding if exists. And ignore the error if not found.
-  kubectl delete clusterrolebinding clusterconnect-binding --ignore-not-found
-  kubectl create clusterrolebinding clusterconnect-binding --clusterrole=cluster-admin --user="${OBJECT_ID}" 2> "${results_dir}"/error || python3 /arc/setup_failure_handler.py
-
-  kubectl config set-context ${KUBECTL_CONTEXT} \
-    --cluster=${KUBECTL_CONTEXT} \
-    --user=${KUBECTL_CONTEXT} \
-    --namespace=default 2> "${results_dir}"/error || python3 /arc/setup_failure_handler.py
-
-  kubectl config use-context ${KUBECTL_CONTEXT} 2> "${results_dir}"/error || python3 /arc/setup_failure_handler.py
-  echo "INFO: KubeConfig setup complete"
-}
-
-# validate enviorment variables
-validateParameters() {
-  if [ -z "${TENANT_ID}" ]; then
-    echo "ERROR: parameter TENANT_ID is required." > "${results_dir}"/error
-    python3 /arc/setup_failure_handler.py
-  fi
-
-  if [ -z "${SUBSCRIPTION_ID}" ]; then
-    echo "ERROR: parameter SUBSCRIPTION_ID is required." > "${results_dir}"/error
-    python3 /arc/setup_failure_handler.py
-  fi
-
-  if [ -z "${CLUSTER_NAME}" ]; then
-    echo "ERROR: parameter CLUSTER_NAME is required." > "${results_dir}"/error
-    python3 /arc/setup_failure_handler.py
-  fi
-
-  if [ -z "${CLUSTER_RG}" ]; then
-    echo "ERROR: parameter CLUSTER_RG is required." > "${results_dir}"/error
-    python3 /arc/setup_failure_handler.py
-  fi
-
-  # OBJECT_ID is an id of the Service Principal created in conformance test subscription.
-  if [ -z "${OBJECT_ID}" ]; then
-    echo "ERROR: parameter OBJECT_ID is required." > "${results_dir}"/error
-    python3 /arc/setup_failure_handler.py
-  fi
-
-  if [[ -z "${WORKLOAD_CLIENT_ID}" ]]; then
-    if [ -z "${AZURE_CLIENT_ID}" ]; then
-      echo "ERROR: parameter AZURE_CLIENT_ID is required." > "${results_dir}"/error
-      python3 /arc/setup_failure_handler.py
-    fi
-
-    if [ -z "${AZURE_CLIENT_SECRET}" ]; then
-      echo "ERROR: parameter AZURE_CLIENT_SECRET is required." > "${results_dir}"/error
-      python3 /arc/setup_failure_handler.py
-    fi
-   fi
-}
-
-login_to_azure() {
-   if [[ -z $WORKLOAD_CLIENT_ID ]]; then
-      echo "logging in using service principal '${AZURE_CLIENT_ID}'"
-      az login --service-principal \
-         -u ${AZURE_CLIENT_ID} \
-         -p ${AZURE_CLIENT_SECRET} \
-         --tenant ${TENANT_ID} 2> ${results_dir}/error || python3 setup_failure_handler.py
-   else
-      echo "logging in using managed identity '${WORKLOAD_CLIENT_ID}'"
-      az login --identity \
-         -u ${WORKLOAD_CLIENT_ID} 2> ${results_dir}/error || python3 setup_failure_handler.py
-   fi
-
-	echo "setting subscription: ${SUBSCRIPTION_ID} as default subscription"
-	az account set -s $SUBSCRIPTION_ID
-}
-
+# validate parameters
 validateParameters
 
-# add az cli extensions 
-az extension add --name aks-preview
-az extension add --name k8s-extension
-
+# login to azure
 login_to_azure
 
-# setup Kubeconfig
-setupKubeConfig
+# add k8s-extension extension
+addK8sExtension
 
-# Wait for resources in ARC agents to come up
-echo "INFO: Waiting for ConnectedCluster to come up"
-waitSuccessArc="$(waitForArc)"
-if [ "${waitSuccessArc}" == false ]; then
-    echo "helm release azure-arc failed" > "${results_dir}"/error
-    python3 /arc/setup_failure_handler.py
-    exit 1
-else
-    echo "INFO: ConnectedCluster is available"
-fi
+# create k8s provider extension
+createK8sProviderExtension
 
-# register the KubernetesConfiguration resource provider
-az provider register --namespace Microsoft.KubernetesConfiguration
+# wait for k8s provider extension to be installed
+waitForK8sProviderExtensionInstalled
 
-echo "INFO: Creating extension"
-az k8s-extension create \
-      --name appconfigurationkubernetesprovider \
-      --extension-type Microsoft.AppConfiguration \
-      --cluster-name "${CLUSTER_NAME}" \
-      --resource-group "${CLUSTER_RG}" \
-      --cluster-type managedClusters \
-      --release-train preview 2> "${results_dir}"/error || python3 /arc/setup_failure_handler.py
-
-# wait for provider pods
-kubectl wait pod -n azappconfig-system --for=condition=Ready -l app.kubernetes.io/instance=azureappconfiguration.kubernetesprovider --timeout=5m
-
-# clean up test resources
-echo "INFO: cleaning up test resources" 
-az k8s-extension delete \
-  --name appconfigurationkubernetesprovider \
-  --resource-group "${CLUSTER_RG}" \
-  --cluster-type managedClusters \
-  --cluster-name "${CLUSTER_NAME}" \
-  --force \
-  --yes \
-  --no-wait
+# delete k8s provider extension
+deleteK8sProviderExtension

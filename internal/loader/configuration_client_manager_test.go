102f78e15e3d3a453d7ecdafe27c28a25b42bd07
Lingling Ye (from Dev Box) <linglingye@microsoft.com>
2026-03-12 18:05:30 +0800

    fix lint

--- internal/loader/configuration_client_manager_test.go
+++ internal/loader/configuration_client_manager_test.go
@@ -64,7 +64,9 @@ func TestNewClientOptions(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			// Clean up the env var before each test
-			os.Unsetenv(AzureAppConfigAudience)
+			if err := os.Unsetenv(AzureAppConfigAudience); err != nil {
+				t.Fatalf("Failed to unset environment variable: %v", err)
+			}
 
 			// Setup environment variables
 			if tt.envVars != nil {
